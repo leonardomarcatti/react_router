6c9302ff1d0756d9cfc8454d2ebf3f13b8c24a73
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2026-02-03 16:59:09 -0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Error from './pages/Error'
+
+const routes = createBrowserRouter([
+   {path: '/', index: true, element: <Home />, errorElement: <Error />},
+   {path: '/products', element: <Products />},
+])
 
 const App = () => {
-  return <>
-   <h1></h1>
-  </>
+  return <RouterProvider router={routes}/>
 }
 
 export default App
---
+++ src/pages/Error.jsx
@@ -0,0 +1,8 @@
+const Error = () => {
+   return <>
+      <h1>Error Page</h1>
+      <h2>This pare does not exist</h2>
+   </> 
+}
+
+export default Error
