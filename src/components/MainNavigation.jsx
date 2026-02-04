1810a4cd91429562c3277fe6aa1578cbe8f23a78
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2026-02-04 17:22:52 -0300

    useNavigate

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -5,8 +5,8 @@ const MainNavigation = () => {
    return <header className={styles.header}>
       <nav>
          <ul className={styles.list}>
-            <li><NavLink to='/' className={({isActive}) => isActive? styles.active : ''}>Home</NavLink></li>
-            <li><NavLink to='/products' className={({ isActive }) => isActive ? styles.active : ''}>Products</NavLink></li>
+            <li><NavLink to='/' end className={({isActive}) => isActive? styles.active : ''}>Home</NavLink></li>
+            <li><NavLink to='/products' end className={({ isActive }) => isActive ? styles.active : ''}>Products</NavLink></li>
          </ul>
       </nav>
    </header>
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,14 @@
-import MainNavigation from "../components/MainNavigation"
+import { useNavigate } from "react-router-dom"
 
 const Error = () => {
+   const navigate = useNavigate()
+   const handlenavigate = () => navigate('/')
+
    return <main>
-       <MainNavigation />
+
       <h1>Error Page</h1>
       <h2>This pare does not exist</h2>
+      <button type="button" onClick={handlenavigate}>Get Back</button>
    </main> 
 }
 
